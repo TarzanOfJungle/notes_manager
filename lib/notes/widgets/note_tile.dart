a800a33faae3b4c3037e82563a7517e30d3ffa98
Ondrej Nemec <xnemec7@fi.muni.cz>
2024-06-27 21:35:36 +0200

    feat: formatting and displaying timestamp

--- lib/notes/widgets/note_tile.dart
+++ lib/notes/widgets/note_tile.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:notes_manager/common/constants/ui_constants.dart';
 import 'package:notes_manager/notes/models/note.dart';
 
-const _IMPORTANT_COLOR = Color(0xFFFFECB3);
+const _IMPORTANT_NOTE_COLOR = Color(0xFFFFECB3);
+const _DEFAULT_NOTE_COLOR = Color(0xFFE8E8E8);
+
 class NoteTile extends StatelessWidget {
   final Note note;
   final VoidCallback onEdit;
@@ -18,7 +22,7 @@ class NoteTile extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15.0),
-        color: note.isImportant ? _IMPORTANT_COLOR : DefaultSelectionStyle.defaultColor,
+        color: note.isImportant ? _IMPORTANT_NOTE_COLOR : _DEFAULT_NOTE_COLOR,
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -27,15 +31,38 @@ class NoteTile extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(note.title, style: const TextStyle(fontWeight: FontWeight.bold),),
-                IconButton(onPressed: onEdit, icon: const Icon(Icons.edit), tooltip: 'Edit',),
-                IconButton(onPressed: onDelete, icon: const Icon(Icons.delete), tooltip: 'Delete'),
+                Text(
+                  note.title,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                IconButton(
+                  onPressed: onEdit,
+                  icon: const Icon(Icons.edit),
+                  tooltip: 'Edit',
+                ),
+                IconButton(
+                    onPressed: onDelete,
+                    icon: const Icon(Icons.delete),
+                    tooltip: 'Delete'),
               ],
             ),
             if (note.description != null && note.description!.isNotEmpty)
               Text(note.description!),
+            _buildTimestampDisplay(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildTimestampDisplay() {
+    final df = DateFormat('dd-MM-yyyy hh:mm a');
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Text(note.updatedAt == null
+          ? 'Created at: ${df.format(note.createdAt)}'
+          : 'Updated at: ${df.format(note.updatedAt!)}',
+        style: const TextStyle(fontSize: UiConstants.smallFontSize),
       ),
     );
   }
