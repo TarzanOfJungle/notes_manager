c5c5afb6910399fd00edf4ddd37829e371e2de2b
Ondrej Nemec <xnemec7@fi.muni.cz>
2024-06-27 14:25:29 +0200

    feat: implemented notes adding

--- lib/notes/screens/notes_page.dart
+++ lib/notes/screens/notes_page.dart
@@ -3,11 +3,25 @@ import 'package:notes_manager/notes/widgets/note_tile.dart';
 
 import '../models/note.dart';
 
-class NotesPage extends StatelessWidget {
-  NotesPage({super.key});
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
 
-  final List<Note> notes = [
-    Note(title: 'Note 1', isImportant: true, isResolved: false, description: "blabla"),
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  final _newNoteTitleController = TextEditingController();
+
+  final _newNoteDescriptionController = TextEditingController();
+
+  bool _newNoteIsImportant = false;
+
+  final List<Note> _notes = [
+    Note(title: 'Note 1',
+        isImportant: true,
+        isResolved: false,
+        description: "blabla"),
     Note(title: "NoteNote", isImportant: false, isResolved: false)
   ];
 
@@ -19,7 +33,32 @@ class NotesPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          children: notes.map((note) => NoteTile(onEdit: () => {}, note: note)).toList(),
+          children: [
+            const Text("New Note:"),
+            TextField(
+              controller: _newNoteTitleController,
+              decoration: const InputDecoration(labelText: "Title:"),
+
+            ),
+            TextField(
+              controller: _newNoteDescriptionController,
+              decoration: const InputDecoration(labelText: "Description:"),
+            ),
+            Switch(value: _newNoteIsImportant,
+                onChanged: (_) => setState(() {
+                  _newNoteIsImportant = !_newNoteIsImportant;
+                })),
+            ElevatedButton(
+                onPressed: () => setState(() {
+                  _notes.add(Note(title: _newNoteTitleController.text,
+                      description: _newNoteDescriptionController.text,
+                      isImportant: _newNoteIsImportant,
+                      isResolved: false));
+                }),
+                child: const Text("Add Note")),
+            ..._notes.map((note) => NoteTile(onEdit: () => {}, note: note))
+                .toList(),
+          ],
         ),
       ),
     );
